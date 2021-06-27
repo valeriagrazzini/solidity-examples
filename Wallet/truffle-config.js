0cfabcf37a1576fe2fac2093869b41caa9d929e7
valeriagrazzini <vgrazzini@gmail.com>
2021-06-27 17:30:13 +0200

    deploy on TESTNET KOVAN

--- Wallet/truffle-config.js
+++ Wallet/truffle-config.js
@@ -1,4 +1,10 @@
 const path = require('path')
+const provider = require('@truffle/hdwallet-provider')
+const fs = require('fs');
+const secrets = JSON.parse(
+  fs.readFileSync('secrets.json').toString().trim()
+);
+
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
@@ -38,6 +44,18 @@ module.exports = {
   contracts_build_directory: path.join(__dirname, "./client/src/contracts"),
 
   networks: {
+    kovan: {
+      provider: () => {
+        return new provider(secrets.privateKeys,
+          'https://kovan.infura.io/v3/1cadf1e30dc847b3a5d3cd80f48bc9bc',
+          0,
+          3
+        )
+      },
+      network_id: 42,
+      gas: 3000000,
+      gasPrice: 10000000000
+    }
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
     // You should run a client (like ganache-cli, geth or parity) in a separate terminal
